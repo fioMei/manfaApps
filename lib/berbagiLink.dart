10c4deba9be64fcc371c5db68f4481e2d432a5ad
fioMei <muhammadfathurrahman012@gmail.com>
2023-01-01 10:25:06 +0700

    membuat menu berbagi.link di tabbar

--- lib/berbagiLink.dart
+++ lib/berbagiLink.dart
@@ -7,8 +7,38 @@ import 'package:manfa_apps/routes/trackingPixels.dart';
 
 void main() => runApp(berbagiLink());
 
-class berbagiLink extends StatelessWidget {
-  const berbagiLink({super.key});
+class berbagiLink extends StatefulWidget {
+  IconData? icon;
+
+  berbagiLink({Key? key}) : super(key: key);
+
+  @override
+  State<berbagiLink> createState() => _BerbagiLinkState();
+}
+
+final List<Tab> myTabs = List.generate(
+  2,
+  (index) => Tab(text: 'TAB $index'),
+);
+
+class _BerbagiLinkState extends State<berbagiLink>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  // ignore: prefer_typing_uninitialized_variables
+  var kTitleTextStyle;
+
+  @override
+  void initState() {
+    _tabController = TabController(length: 2, vsync: this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,8 +75,7 @@ class berbagiLink extends StatelessWidget {
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(40 / 2),
                     image: DecorationImage(
-                      image: NetworkImage(
-                          "https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2022/06/30/692203986.jpg"),
+                      image: AssetImage("assets/photoprofile.jpg"),
                     ),
                   ),
                 ),
@@ -153,206 +182,231 @@ class berbagiLink extends StatelessWidget {
             // Link end
 
             // tabbar ceritanya tapi ga bisa diteken :)
-            Container(
-              margin: EdgeInsets.only(top: 20, bottom: 20),
-              // padding: EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(32),
-                  border: Border.all(color: Colors.black)
-                  // color: Colors.black,
-                  ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  SizedBox(),
-                  Container(
-                    padding: EdgeInsets.all(16),
-                    margin: EdgeInsets.only(top: 0, bottom: 0),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(32),
-                      // color: ColorSelect.secondaryColor,
-                    ),
-                    child: Row(
-                      children: [
-                        Container(
-                          child: Text(
-                            "Dashboard",
-                            style: TextStyle(
-                              fontWeight: FontWeight.w500,
-                              fontSize: 16,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.fromLTRB(26, 18, 26, 18),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(32),
-                      color: Colors.black,
-                    ),
-                    child: Row(
-                      children: [
-                        Container(
-                          child: Text(
-                            "Menu Berbagi.link",
-                            style: TextStyle(
-                                fontWeight: FontWeight.w100,
-                                fontSize: 16,
-                                color: Colors.white),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            // Container(
+            //   margin: EdgeInsets.only(top: 20, bottom: 20),
+            //   // padding: EdgeInsets.all(16),
+            //   decoration: BoxDecoration(
+            //       borderRadius: BorderRadius.circular(32),
+            //       border: Border.all(color: Colors.black)
+            //       // color: Colors.black,
+            //       ),
+            //   child: Row(
+            //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //     children: [
+            //       SizedBox(),
+            //       Container(
+            //         padding: EdgeInsets.all(16),
+            //         margin: EdgeInsets.only(top: 0, bottom: 0),
+            //         decoration: BoxDecoration(
+            //           borderRadius: BorderRadius.circular(32),
+            //           // color: ColorSelect.secondaryColor,
+            //         ),
+            //         child: Row(
+            //           children: [
+            //             Container(
+            //               child: Text(
+            //                 "Dashboard",
+            //                 style: TextStyle(
+            //                   fontWeight: FontWeight.w500,
+            //                   fontSize: 16,
+            //                 ),
+            //               ),
+            //             ),
+            //           ],
+            //         ),
+            //       ),
+            //       Container(
+            //         padding: EdgeInsets.fromLTRB(26, 18, 26, 18),
+            //         decoration: BoxDecoration(
+            //           borderRadius: BorderRadius.circular(32),
+            //           color: Colors.black,
+            //         ),
+            //         child: Row(
+            //           children: [
+            //             Container(
+            //               child: Text(
+            //                 "Menu Berbagi.link",
+            //                 style: TextStyle(
+            //                     fontWeight: FontWeight.w100,
+            //                     fontSize: 16,
+            //                     color: Colors.white),
+            //               ),
+            //             ),
+            //           ],
+            //         ),
+            //       ),
+            //     ],
+            //   ),
+            // ),
             // tabbar end
 
-            // TabBar(tabs: [
-            //   Tab(
-            //     icon: Icon(Icons.dashboard),
-            //   )
-            // ]),
-
-            // TabBarView(children: []),
-
-            // menu start
-            Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+            TabBar(
+              padding: EdgeInsets.only(top: 20),
+              // isScrollable: true,
+              unselectedLabelColor: Colors.black,
+              labelColor: Colors.white,
+              indicatorWeight: 2,
+              indicator: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.circular(64),
+              ),
+              overlayColor:
+                  MaterialStateProperty.all(Color.fromARGB(0, 0, 0, 0)),
+              tabs: [
+                Tab(
+                  text: 'Dashboard',
+                ),
+                Tab(
+                  text: 'Berbagi.link',
+                )
+              ],
+              controller: _tabController,
+              indicatorSize: TabBarIndicatorSize.tab,
+            ),
+            Expanded(
+              child: TabBarView(
                 children: [
-                  itemKategori(
-                    name: "Ubah Tampilan",
-                    icon: ColorSelect.disableColor,
-                    tap: () {
-                      print("button 1 clicked");
-                    },
-                  ),
-                  itemKategori(
-                    name: "Analisis Link",
-                    icon: ColorSelect.disableColor,
-                    tap: () {
-                      print("button 2 clicked");
-                    },
-                  ),
-                  itemKategori(
-                    name: "Pratinjau",
-                    icon: ColorSelect.disableColor,
-                    tap: () {
-                      print("button 3 clicked");
-                    },
-                  ),
-                  itemKategori(
-                    name: "Tema",
-                    icon: ColorSelect.disableColor,
-                    tap: () {
-                      print("button 4 clicked");
-                    },
+                  Container(child: Text('people')),
+                  Column(
+                    children: [
+                      Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            itemKategori(
+                              name: "Ubah Tampilan",
+                              icon: ColorSelect.disableColor,
+                              tap: () {
+                                print("button 1 clicked");
+                              },
+                            ),
+                            itemKategori(
+                              name: "Analisis Link",
+                              icon: ColorSelect.disableColor,
+                              tap: () {
+                                print("button 2 clicked");
+                              },
+                            ),
+                            itemKategori(
+                              name: "Pratinjau",
+                              icon: ColorSelect.disableColor,
+                              tap: () {
+                                print("button 3 clicked");
+                              },
+                            ),
+                            itemKategori(
+                              name: "Tema",
+                              icon: ColorSelect.disableColor,
+                              tap: () {
+                                print("button 4 clicked");
+                              },
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 20),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            itemKategori(
+                              name: "Tracking Pixels",
+                              icon: ColorSelect.disableColor,
+                              tap: () {
+                                print("button 5 clicked");
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) {
+                                      return trackingPixels();
+                                    },
+                                  ),
+                                );
+                              }
+                            ),
+                            itemKategori(
+                              name: "Email Database",
+                              icon: ColorSelect.disableColor,
+                              tap: () {
+                                print("button 6 clicked");
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) {
+                                      return emailDatabase();
+                                    },
+                                  ),
+                                );
+                              },
+                            ),
+                            itemKategori(
+                              name: "Tagihan",
+                              icon: ColorSelect.disableColor,
+                              tap: () {
+                                print("button 7 clicked");
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) {
+                                      return tagihan();
+                                    },
+                                  ),
+                                );
+                              },
+                            ),
+                            itemKategori(
+                              name: "Bagi.to",
+                              icon: ColorSelect.disableColor,
+                              tap: () {
+                                print("button 8 clicked");
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 20),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            itemKategori(
+                              name: "Manajemen\nUser",
+                              icon: ColorSelect.disableColor,
+                              tap: () {
+                                print("button 9 clicked");
+                              },
+                            ),
+                            itemKategori(
+                              name: "Event Online",
+                              icon: ColorSelect.disableColor,
+                              tap: () {
+                                print("button 10 clicked");
+                              },
+                            ),
+                            itemKategori(
+                              name: "Profile",
+                              icon: ColorSelect.disableColor,
+                              tap: () {
+                                print("button 11 clicked");
+                              },
+                            ),
+                            itemKategori(
+                              name: "Integrasi Pihak\nketiga",
+                              icon: ColorSelect.disableColor,
+                              tap: () {
+                                print("butto12 cliked");
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                 ],
+                controller: _tabController,
               ),
             ),
-            Container(
-              margin: EdgeInsets.only(top: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  itemKategori(
-                    name: "Tracking Pixels",
-                    icon: ColorSelect.disableColor,
-                    tap: () {
-                      print("button 5 clicked");
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) {
-                            return trackingPixels();
-                          },
-                        ),
-                      );
-                    },
-                  ),
-                  itemKategori(
-                    name: "Email Database",
-                    icon: ColorSelect.disableColor,
-                    tap: () {
-                      print("button 6 clicked");
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) {
-                            return emailDatabase();
-                          },
-                        ),
-                      );
-                    },
-                  ),
-                  itemKategori(
-                    name: "Tagihan",
-                    icon: ColorSelect.disableColor,
-                    tap: () {
-                      print("button 7 clicked");
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) {
-                            return tagihan();
-                          },
-                        ),
-                      );
-                    },
-                  ),
-                  itemKategori(
-                    name: "Bagi.to",
-                    icon: ColorSelect.disableColor,
-                    tap: () {
-                      print("button 8 clicked");
-                    },
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  itemKategori(
-                    name: "Manajemen\nUser",
-                    icon: ColorSelect.disableColor,
-                    tap: () {
-                      print("button 9 clicked");
-                    },
-                  ),
-                  itemKategori(
-                    name: "Event Online",
-                    icon: ColorSelect.disableColor,
-                    tap: () {
-                      print("button 10 clicked");
-                    },
-                  ),
-                  itemKategori(
-                    name: "Profile",
-                    icon: ColorSelect.disableColor,
-                    tap: () {
-                      print("button 11 clicked");
-                    },
-                  ),
-                  itemKategori(
-                    name: "Integrasi Pihak\nketiga",
-                    icon: ColorSelect.disableColor,
-                    tap: () {
-                      print("butto12 cliked");
-                    },
-                  ),
-                ],
-              ),
-            ),
-            // menu end
           ],
         ),
       ),
